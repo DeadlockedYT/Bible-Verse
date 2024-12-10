a6c36be4e562f99b399880a3292c9eb1663d4f26
DeadlockedYT <131408411+DeadlockedYT@users.noreply.github.com>
2024-12-10 14:32:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let x;
 document.getElementById("generate").onclick =  function(){
-    x = (Math.random() * 100) + 1;
+    x = (int)((Math.random() * 100) + 1);
     document.getElementById("xlabel").innerHTML = x;
 }
