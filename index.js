347dcdc8cd421c0bdb54a08947068ba22d829dc7
DeadlockedYT <131408411+DeadlockedYT@users.noreply.github.com>
2024-12-10 14:42:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let x;
 document.getElementById("generate").onclick =  function(){
     x = ((Math.random() * 100) + 1);
-    x = Math.round(x);
+   // x = Math.round(x);
     document.getElementById("xlabel").innerHTML = x;
 }
